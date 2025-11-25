c654dcf8880188dbec6f2b6dacd32efa06d4961b
mohamed keytsane <osamakeysane20@gmail.com>
2025-11-25 10:17:08 +0300

    Add Navbar component with logo, search bar, cart, and user buttons

--- E-Commerce Product Catalog/src/components/Navbar.tsx
+++ E-Commerce Product Catalog/src/components/Navbar.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg=white shadow-md">
+      <div className="container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center">
+        <div className="text-lg font-bold">
+          <Link to="/">eShop</Link>
+        </div>
+        <div className="relative flex-1 mx-4">
+          <form>
+            <input
+              type="text"
+              placeholder="Search Product"
+              className="w-full border py-2 px-4"
+            />
+            <FaSearch className="absolute top-3 right-3 text-red-500"></FaSearch>
+          </form>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link to="/cart">
+            <FaShoppingCart className="text-lg" />
+          </Link>
+          <button className="hidden md:block">Login | Register</button>
+          <button className="block">
+            <FaUser />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
