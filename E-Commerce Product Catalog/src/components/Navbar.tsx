23a5a271780e1e188dc1fb1549bc915143d7348a
mohamed keytsane <osamakeysane20@gmail.com>
2025-11-26 12:09:37 +0300

    display cart item count badge in navbar using Redux state

--- E-Commerce Product Catalog/src/components/Navbar.tsx
+++ E-Commerce Product Catalog/src/components/Navbar.tsx
@@ -1,10 +1,12 @@
-import React from "react";
 import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import type { RootState } from "../redux/store";
 
 const Navbar = () => {
+  const products = useSelector((state: RootState) => state.cart.products);
   return (
-    <nav className="bg=white shadow-md">
+    <nav className="bg-white shadow-md">
       <div className="container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center">
         <div className="text-lg font-bold">
           <Link to="/">eShop</Link>
@@ -20,8 +22,13 @@ const Navbar = () => {
           </form>
         </div>
         <div className="flex items-center space-x-4">
-          <Link to="/cart">
+          <Link to="/cart" className="relative">
             <FaShoppingCart className="text-lg" />
+            {products.length > 0 && (
+              <span className="absolute top-0 text-xs w-3 left-3 bg-red-600 rounded-full flex justify-center items-center text-white">
+                {products.length}
+              </span>
+            )}
           </Link>
           <button className="hidden md:block">Login | Register</button>
           <button className="block">
